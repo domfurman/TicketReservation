7d8ae2c220b551ed9cf5fe8ae53282d66a1de79c
Dominik Furman <s222915@student.uek.krakw.pl>
2024-01-23 17:09:55 +0100

    rewriting controllers to handle cross origin

--- TicketReservation/src/main/java/pl/ticketreservation/screening/ScreeningController.java
+++ TicketReservation/src/main/java/pl/ticketreservation/screening/ScreeningController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import pl.ticketreservation.seat.seatrepository.JdbcSeatRepository;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ScreeningController {
     private final ScreeningService screeningService;
     private final JdbcSeatRepository jdbcSeatRepository;
