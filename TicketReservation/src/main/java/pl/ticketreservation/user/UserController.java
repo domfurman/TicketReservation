d3b5ab675e318add24bcc562a2ae072183b142d5
Dominik Furman <s222915@student.uek.krakw.pl>
2024-02-05 18:01:13 +0100

    cross origin user

--- TicketReservation/src/main/java/pl/ticketreservation/user/UserController.java
+++ TicketReservation/src/main/java/pl/ticketreservation/user/UserController.java
@@ -2,15 +2,13 @@ package pl.ticketreservation.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.ticketreservation.user.userrepository.JdbcUserRepository;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final JdbcUserRepository jdbcUserRepository;
